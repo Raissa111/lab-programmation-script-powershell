9fe3364721797d5b919976dc3712948f6e6cefa4
Raissa111 <ernanjia@gmail.com>
2020-04-13 12:41:54 -0400

    Embellir son bonjour

--- b000000000-fonction.ps1
+++ b000000000-fonction.ps1
@@ -6,14 +6,16 @@
     Ce script est utilisé pour le laboratoire de programmation en Powershell.
 
 .NOTES
-    Author: CollegeBoreal
+    Author: Raissa111
     Derniere mise à jour: yyyy-mm-dd
 
 #>
 
 # Definition de la fonction
 function Stagiaire {
-
+$personneNom = "Alice"
+$personneAge = 35
+Write-Host "Bonjour $personneNom, tu as $personneAge ans. "
 }
 
 # Appel de la fonction
